c3a872fcb3d4e0ee90e4641893d70590b66b05f8
winkwink <amandawinkler44@gmail.com>
2019-11-25 19:35:24 -0500

    finished

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ var imagesToBeClicked = document.getElementsByClassName("img-class");
 
 for(var i = 0, max = imagesToBeClicked.length; i < max; i += 1) {
   var clickedImg = imagesToBeClicked[i];
-  clickedImg.onclick=function (){
-    toggleClass(this);
+    clickedImg.onclick=function (){
+    createImg(this);
   };
 }
 
-function  toggleClass(element){
-  element.classList.toggle("expand-img"); 
+function  createImg(element){
+   var img = document.createElement('IMG');
+   var anchor = img.id = "jump";
+   var imgSrc = img.src = element.src;
+   img.classList.add("expand");
+   var x = document.createElement('IMG');
+   xSrc = x.src = 'images/x.png';
+   x.classList.add('x-style');
+   document.getElementById('pop-up-img').appendChild(img);
+   document.getElementById('pop-up-img').appendChild(x);
+
+   x.onclick = function(){
+   	var popUpDiv = document.getElementById('pop-up-img');
+   	popUpDiv.removeChild(img);
+   	popUpDiv.removeChild(x);
+   }
 }
+
+
